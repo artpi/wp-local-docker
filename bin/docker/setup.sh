f5b351bbc0dc61614278b51288760431660b7be3
artpi <artur.piszek@gmail.com>
2018-12-29 15:04:06 +0100

    Some changes

--- bin/docker/setup.sh
+++ bin/docker/setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 cd /var/www/html
-#wp core download --skip-themes --skip-plugins
+if [ -f /var/www/html/xmlrpc.php ]
+then
+	echo "skipping wg install\n"
+elif [ -f /var/www/html/wp-content/themes ]
+then
+	wp core download --skip-content
+else
+	wp core download
+fi
+
 rm wp-config.php
 wp core config --dbhost=mysql --dbname=wordpress --dbuser=root --dbpass=password
 wp core install --url=localhost --title="Test site" --admin_user=admin --admin_email=artur.piszek@gmail.com --admin_password=password
